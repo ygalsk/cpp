a443b9d08e7234977d3ee252ac17189f6f7af25c
dkremer <dkremer@dkremer.fritz.box>
2025-04-13 15:19:49 +0200

    Headerfile for Intern class

--- cpp_05/ex03/inc/Intern.hpp
+++ cpp_05/ex03/inc/Intern.hpp
@@ -1,3 +1,21 @@
 #pragma once
 
+#include <iostream>
 
+#include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+
+class Intern {
+public:
+		Intern();
+		Intern(Intern const &other);
+		Intern &operator=(Intern const &other);
+		~Intern();
+		AForm *makeForm(std::string formName, std::string target);
+};
+
+AForm *createPresidentialPardonForm(std::string target);
+AForm *createRobotomyRequestForm(std::string target);
+AForm *createShrubberyCreationForm(std::string target);
