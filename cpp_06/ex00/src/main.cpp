c88488711d9b1a83d171b5fc3cc840f4a710ab5a
Daniel Kremer <dkremer@1-A-28.42heilbronn.de>
2025-04-15 17:14:06 +0200

    main function for testing

---
+++ cpp_06/ex00/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Converter.hpp>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2){
+        std::cout << "Usage: ./convert <input>" << std::endl;
+        return 1;
+    }
+    Converter::convert(argv[1]);
+    return 0;
+}
