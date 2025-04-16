653cfcdc5666ab1eb5ce0d30b4a6316934619e8a
Daniel Kremer <dkremer@1-D-15.42heilbronn.de>
2025-04-16 14:33:29 +0200

    feat: util functions to check what input type it is

--- cpp_06/ex00/src/utils.cpp
+++ cpp_06/ex00/src/utils.cpp
@@ -12,3 +12,57 @@ static eType getType(std::string str) {
 	else
 		return INVALID;
 }
+
+bool isChar(std::string str) {
+	if (str.length() == 1 && isprint(str[0]))
+		return true;
+	return false;
+}
+
+bool isInt(std::string str) {
+	if (str.length() == 0)
+		return false;
+	if (str[0] == '-' || str[0] == '+')
+		str = str.substr(1);
+	for (size_t i = 0; i < str.length(); i++) {
+		if (!isdigit(str[i]))
+			return false;
+	}
+	return true;
+}
+
+bool isFloat(std::string str) {
+	if (str.length() == 0)
+		return false;
+	if (str[0] == '-' || str[0] == '+')
+		str = str.substr(1);
+	size_t dotCount = 0;
+	for (size_t i = 0; i < str.length(); i++) {
+		if (str[i] == '.') {
+			dotCount++;
+			if (dotCount > 1)
+				return false;
+		} else if (!isdigit(str[i])) {
+			return false;
+		}
+	}
+	return dotCount == 1;
+}
+
+bool isDouble(std::string str) {
+	if (str.length() == 0)
+		return false;
+	if (str[0] == '-' || str[0] == '+')
+		str = str.substr(1);
+	size_t dotCount = 0;
+	for (size_t i = 0; i < str.length(); i++) {
+		if (str[i] == '.') {
+			dotCount++;
+			if (dotCount > 1)
+				return false;
+		} else if (!isdigit(str[i])) {
+			return false;
+		}
+	}
+	return dotCount == 1;
+}
