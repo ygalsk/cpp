6e6539e8a824b77f7561f0c2d4b8224523110b7e
Daniel Kremer <dkremer@1-D-15.42heilbronn.de>
2025-04-16 14:37:41 +0200

    feat(serializer): add Data struct and static serialization methods
    - Introduce Data struct with int and string members
    - Make Serializer class non-instantiable
    - Add static serialize and deserialize methods

--- cpp_06/ex01/inc/Serializer.hpp
+++ cpp_06/ex01/inc/Serializer.hpp
@@ -2,6 +2,18 @@
 
 #include <iostream>
 
+struct Data {
+	int rnd_int;
+	std::string rnd_string;
+};
 
 class Serializer {
+public:
+		Serializer() = delete;
+		Serializer(const Serializer &other) = delete;
+		Serializer &operator=(const Serializer &other) = delete;
+		~Serializer() = delete;
+
+		static uintptr_t serialize(Data *ptr);
+		static Data *deserialize(uintptr_t raw);
 };
